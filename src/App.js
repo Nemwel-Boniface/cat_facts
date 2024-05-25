d636b689ce689ee7997c5d3dd162e92af9a473d8
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-05-25 08:03:34 +0300

    Update included:   - INstall the Bootstrap package to the project   - Create the Navbar and gave it Bootsrap classes for styling   - Import the Navbar to the main project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
