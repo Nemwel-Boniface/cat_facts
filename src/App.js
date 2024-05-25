c99ac3da5c3625d199720a16d64f8bc3ac0cdab0
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-05-25 08:29:47 +0300

    Update included:   - Update the project title and add font awesome CDN links   - Create the footer section and link it to the main project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>    
   );
 }
