196a1a18a5a9cc9b7991bab9cdf759b6140908b3
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-05-26 14:16:35 +0300

    Update made the navbar links display depending on whether user is logged in or not

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <header className="navbar">
+const Navbar = () => {
+  const isUserLoggedIn = useSelector((state) => state.login.isLoggedIn);
+
+  return (
+    <header className="navbar">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">Cat Facts</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,16 +14,34 @@ const Navbar = () => (
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/login">Login</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/signup">SignUp</Link>
-          </li>
+          {
+            isUserLoggedIn
+              ? (
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/welcome">Welcome</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Log Out</Link>
+                </li>
+              </ul>
+            ):
+            (
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item active">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">SignUp</Link>
+                </li>
+              </ul>
+            )
+          }
         </ul>
       </div>
     </nav>
   </header>
-);
+  )
+};
 
 export default Navbar;
